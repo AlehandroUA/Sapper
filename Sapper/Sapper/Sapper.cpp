0cb45c7cb80f02ee2a2308b7a8394b11ea99f0e5
Alexandrus Mechanicus <sologub.sasha2001@gmail.com>
2024-05-23 20:18:03 +0300

    fix324

--- Sapper/Sapper/Sapper.cpp
+++ Sapper/Sapper/Sapper.cpp
@@ -619,8 +619,9 @@ void fieldFilling(int difficulty, int height, int width) {
     fieldCreating(fieldMines, fieldView, height, width);
 
     do {
-        /*i = rand() % height;
-        j = rand() % width;*/
+        srand(time(NULL));
+        i = rand() % height;
+        j = rand() % width;
 
         if ((fieldMines[i][j] >= 0 && fieldMines[i][j] <= Mine - 1) && fieldMines[i][j] != Mine) {
             fieldMines[i][j] = Mine;
